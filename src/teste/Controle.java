fbb37267baf237d12db95c6d4cad29edcb02fdba
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-03-22 08:49:31 -0300

    inseridos getters, setters e contructors

--- src/teste/Controle.java
+++ src/teste/Controle.java
@@ -9,12 +9,47 @@ public class Controle {
 
     protected boolean ligado;
 
+    public Controle() {
+        this.setModelo("Panasonic");
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
 
     public void status(){
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Volume: " + this.volume);
-        System.out.println("Canal: " + this.canal);
-        System.out.println("Está ligado?  " + this.ligado);
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Volume: " + this.getVolume());
+        System.out.println("Canal: " + this.getCanal());
+        System.out.println("Está ligado?  " + this.isLigado());
     }
 
     protected void ligar()  {
--- src/teste/Main.java
+++ src/teste/Main.java
@@ -3,12 +3,25 @@ package teste;
 public class Main {
     public static void main(String[] args) {
         Controle c1 = new Controle();
-
-        c1.modelo = "Panasonic";
-        c1.canal = 13;
-        c1.volume = 36;
+        c1.setCanal(13);
+        c1.setVolume(30);
         c1.ligar();
         c1.status();
+
+        Controle c2 = new Controle();
+        c2.setModelo("Sony");
+        c2.setCanal(2);
+        c2.setVolume(40);
+        c2.ligar();
+        c2.status();
+
+        Controle c3 = new Controle();
+        c3.setCanal(11);
+        c3.setVolume(20);
+        c3.ligar();
+        c3.status();
+
+
 
 
     }
